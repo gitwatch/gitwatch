450572230f08ed342e306d9c495272a93da54166
Dave Musicant (home machine) <dmusicant@carleton.edu>
2022-12-04 09:21:42 -0600

    included startup/shutdown

---
+++ othertests/startup-shutdown.bash
@@ -0,0 +1,35 @@
+setup() {
+  # Time to wait for gitwatch to respond
+  # shellcheck disable=SC2034
+  WAITTIME=4
+  # Set up directory structure and initialize remote
+  testdir=$(mktemp -d)
+  # shellcheck disable=SC2164
+  cd "$testdir"
+  mkdir remote
+  # shellcheck disable=SC2164
+  cd remote
+  git init -q --bare
+  # shellcheck disable=SC2103
+  cd ..
+  # shellcheck disable=SC2164
+  mkdir local
+  # shellcheck disable=SC2164
+  cd local
+  git clone -q ../remote
+}
+
+teardown() {
+  echo '# Teardown started' >&3
+  # Remove testing directories
+  # shellcheck disable=SC2164
+  cd /tmp
+
+  #    rm -rf $testdir
+
+  # Make sure gitwatch script gets killed if script stopped background
+  # Must kill the entire tree of processes generated
+  pkill -15 -P "$GITWATCH_PID"
+
+  echo '# Teardown complete' >&3
+}
