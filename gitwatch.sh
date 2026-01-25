f21d1e9d41e5235ed7d012d8616a0158f9c2a623
madeupandprobablydoesnotexist <justlivinginashell@gmail.com>
2026-01-25 10:40:20 -0500

    Update gitwatch.sh (#156)
    * Update gitwatch.sh
    
    For some reason, inotifywait has weird quirks with exclusions; while they may work on first iteration (why?), further changes may lead to a situation in which:
    1) User makes change to file inside target directory
    2) Initial commit is made
    3) ".git/objects/" is detected as changed, despite .git's exclusion
    4) Second commit occurs
    5) Before push is possible, status is checked; this causes an unfavorable status of  "No tracked changes detected." 
    6) The script has lost a race against itself within this window and does not go through with the push.
    7) As this message is not seen unless verbose is on, it looks as if the commit was successful, but it will not reach the remote git.
    
    While this is likely an issue with inotifywait's exclude logic, it'd require someone willing to devote that time into a bug that doesn't replicate across systems, and I've sunk enough already.
    
    This issue has been mentioned in https://github.com/gitwatch/gitwatch/issues/115 but wasn't able to be diagnosed and dismissed. However, I've hit this wall as well unfortunately, and I'd like to propose this simple fix so no other poor soul has to sink 4-5 hours into something that should've worked out of the box.
    
    * Update gitwatch.sh
    
    less redundancy
    
    * Update gitwatch.sh
    
    3rd time's a charm
    
    * Backup exclude check against '.git/objects/' time sink.
    
    An added check and tale, to resolve: https://github.com/gitwatch/gitwatch/pull/156
    
    * Update gitwatch.sh
    
    Of course I'd have a typo in the last line.
    https://github.com/gitwatch/gitwatch/pull/156
    
    * Update gitwatch.sh
    
    Attempt to resolve https://github.com/gitwatch/gitwatch/pull/156
    and avoid this situation from reoccurring, as it tends to keep reappearing.
    
    * Update gitwatch.sh
    
    (Safety first.) Attempt to resolve #156 and avoid this situation from reoccurring, as it tends to keep reappearing.

--- gitwatch.sh
+++ gitwatch.sh
@@ -462,6 +462,12 @@ fi
 # Would be great to fix the ignored issue below; ignoring it for now.
 # shellcheck disable=SC2294
 eval "$INW" "${INW_ARGS[@]}" | while read -r line; do
+  # In some distributed version of inotifywait, the exclude pattern is not properly enforced. This
+  #   leads to multiple commits invoked per change and failing to push to remote. (pull #156)
+  if [[ "$line" =~ (^|.*/)\.git(/|$) ]]; then
+    continue
+  fi
+
   verbose_echo "Change detected: $line"
   # is there already a timeout process running?
   if [[ -n $SLEEP_PID ]] && kill -0 "$SLEEP_PID" &> /dev/null; then
